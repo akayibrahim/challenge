58ea401044fa99fc0a64d5489a46aacd3d0e606a
hitchakay <that_see@hotmail.com>
2017-11-25 00:44:35 +0300

    switch embedded mongo to just mongo

--- src/main/java/org/chl/Application.java
+++ src/main/java/org/chl/Application.java
@@ -1,10 +1,16 @@
 package org.chl;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@Configuration
+@EnableAutoConfiguration
 public class Application {
     
     public static void main(String[] args) {
--- src/main/java/org/chl/repos/ChallengeRepository.java
+++ src/main/java/org/chl/repos/ChallengeRepository.java
@@ -1,12 +1,12 @@
 package org.chl.repos;
 
 import org.chl.models.Challenge;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.math.BigInteger;
 
 /**
  * Created by ibrahim on 11/24/2017.
  */
-public interface ChallengeRepository extends CrudRepository<Challenge, BigInteger> {
+public interface ChallengeRepository extends MongoRepository<Challenge, BigInteger> {
 }
